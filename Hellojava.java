b24d0be09d48c8fd42b00191e5d1e4074c482037
elna15 <145443464+elna15@users.noreply.github.com>
2023-09-19 15:08:29 +0530

    My first java project

---
+++ Hellojava.java
@@ -0,0 +1,24 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ File          :Hellojava.java
+ Description   :My first java program "Hello java"
+ Author        :Elna S Bijo
+ Version       :1.0
+ Date          :19/09/2023 
+ */
+
+
+
+
+
+
+
+
+package javalab;
+
+public class Hellojava {
+	public static void main(String [] args) {
+	System.out.println("Hello World"); 
+	System.out.print("Hello Java");
+
+}
+}
